39a5962d81ba5e1373df380f7564489405dc905d
theDasturchi <e-mail pls>
2024-09-07 10:14:14 +0500

    fs module callback function

--- bin_modules/files.js
+++ bin_modules/files.js
@@ -1,13 +1,24 @@
-//file modules in node
-const fileSystem = require("fs");
-const log = require("../utils");
+//* sync file modules in node
+// const fileSystem = require("fs");
+// const log = require("../utils");
 
-const first = fileSystem.readFileSync("./content/first.txt", "utf-8");
-const second = fileSystem.readFileSync("./content/second.txt", "utf-8");
-console.log(first, second);
+// const first = fileSystem.readFileSync("./content/first.txt", "utf-8");
+// const second = fileSystem.readFileSync("./content/second.txt", "utf-8");
+// console.log(first, second);
 
-fileSystem.writeFileSync(
-  "./content/result.txt",
-  `here is the result: ${first}, second: ${second}`,
-  { flag: "a" }
-);
+// fileSystem.writeFileSync(
+//   "./content/result.txt",
+//   `here is the result: ${first}, second: ${second}`,
+//   { flag: "a" }
+// );
+
+// * async file system
+const { error } = require("console");
+const { readFile, writeFile } = require("fs");
+readFile("./content/first.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return 0;
+  }
+  console.log(result);
+});
